265f2e6e53a1d4fbbe908e807891595932419aa1
heeyun <hyelle@naver.com>
2021-09-29 16:56:03 +0900

    ADD solution for #8958

--- BaekJoon/src/BaekJoon/S_8958.java
+++ BaekJoon/src/BaekJoon/S_8958.java
@@ -1,10 +1,46 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
+/*"OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 
+ * 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+
+"OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수가 주어진다. 
+각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다. 
+문자열은 O와 X만으로 이루어져 있다.
+*/
 public class S_8958 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt(); //테스트 케이스 수
+		String[] msg = new String[count];
+		String[] ch=null;
 
+		int sum = 0;
+		int score = 0;
+		
+		for(int i=0;i<count;i++) {
+			msg[i] = sc.next();
+			ch = msg[i].split("");
+			
+			for(int k=0;k<ch.length;k++) {
+				if(ch[k].equals("O")) {
+					score ++;
+					sum += score;
+				}else {
+					score=0;
+					continue;
+				}
+			}
+			System.out.println(sum);
+			sum=0;
+			score=0;
+		}
 	}
-
 }
