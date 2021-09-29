ea3e25289faa95e53ec0044c18d9b1891529b16b
heeyun <hyelle@naver.com>
2021-09-30 00:45:49 +0900

    ADD BaekJoon solution #15596

---
+++ BaekJoon/src/BaekJoon/S_15596.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+/*
+Java: long sum(int[] a); (클래스 이름: Test)
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+리턴값: a에 포함되어 있는 정수 n개의 합
+*/
+public class S_15596 {
+	
+	public class Test {
+	    long sum(int[] a) {
+	        long length=a.length;
+	        long ans = 0;
+	    	for(int i=0;i<length;i++) {
+	    		ans += a[i];
+	    	}
+	        return ans;
+	    }
+	}
+}
