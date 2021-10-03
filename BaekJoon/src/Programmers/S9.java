bddda9a609d3f4a0c69235ac5c8c012b7bd5ee3f
heeyun <hyelle@naver.com>
2021-10-03 21:58:41 +0900

    ADD programmers solution 음양 더하기

---
+++ BaekJoon/src/Programmers/S9.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class S9 {
+	public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]==true){
+                absolutes[i] = absolutes[i]*1;
+            }else if(signs[i]==false){
+                absolutes[i] = absolutes[i]*(-1);
+            }
+            answer += absolutes[i];
+            
+        }
+        
+        return answer;
+    }
+}
