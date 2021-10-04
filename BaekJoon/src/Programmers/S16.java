b87b47ef23a28d6b38b8c1df5e96522d7af82612
heeyun <hyelle@naver.com>
2021-10-05 02:13:27 +0900

    ADD programmers solution 같은 숫자는 싫어!

---
+++ BaekJoon/src/Programmers/S16.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.*;
+
+public class S16 {
+	
+	public int[] solution(int []arr) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(arr[0]);
+		
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]!=arr[i-1]) {
+				list.add(arr[i]);
+			}
+		}
+
+		int[] answer = new int[list.size()];
+		
+		for(int i=0;i<list.size();i++) {
+			answer[i] = list.get(i);
+		}
+		
+        return answer;
+    }
+}
