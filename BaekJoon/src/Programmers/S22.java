c4c52a83c4112f6ea384d5500fb74f06e999a5ad
heeyun <hyelle@naver.com>
2021-10-09 00:39:44 +0900

    ADD programmers 서울에서 김서방 찾기

---
+++ BaekJoon/src/Programmers/S22.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+public class S22 {
+	public static void main(String[] args) {
+		String[] seoul ={"Jane", "Kim"};
+		System.out.println(solution(seoul));
+		
+	}
+	
+	public static String solution(String[] seoul) {
+        String answer = "";
+        int count = 0;
+        
+        for(int i=0;i<seoul.length;i++){
+            if(seoul[i].equals("Kim")){
+                count = i;
+            }
+        }
+        
+        answer = "김서방은 " + count + "에 있다.";
+        
+        return answer;
+    }
+}
