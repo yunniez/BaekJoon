e5f1c80b42eb52bd43f6469958106263fe8d7583
heeyun <hyelle@naver.com>
2021-10-17 11:19:59 +0900

    UPDATE comment 1874

--- BaekJoon/src/BaekJoon/S_1874.java
+++ BaekJoon/src/BaekJoon/S_1874.java
@@ -1,37 +1,43 @@
 package BaekJoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 
 public class S_1874 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int N  = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        //테스트 케이스 갯수
+        int count = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+        //시작 위치
         int start = 1;
-        for(int i = 0; i < N; i++){
+        for(int i=0;i<count;i++){
+        	//데이터 값
             int num = Integer.parseInt(br.readLine());
             if(num >= start){
-                for(int j = start; j <= num; j++){
+                for(int j=start;j<=num;j++){
                     stack.push(j);
-                    sb.append("+\n");
+                    bw.append("+\n");
                 }
-                start = num + 1;
+                start = num+1;
             }
-            else if(num != stack.peek()){
+            else if(num!=stack.peek()){
                 System.out.println("NO");
             }
+            
             while(!stack.empty()){
-                sb.append("-\n");
+                bw.append("-\n");
                 if(stack.pop() == num) break;
             }
         }
-        System.out.println(sb.toString());
-		
+        bw.flush();
+		bw.close();
 	}
 
 }
