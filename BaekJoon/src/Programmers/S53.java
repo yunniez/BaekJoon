f3d8e33a511737059088a7d6c890fa89a4a1ad87
yunnie <hyelle@naver.com>
2021-11-30 00:20:16 +0900

    UPDATE solution for programmers 행렬 테두리 돌리기

--- BaekJoon/src/Programmers/S53.java
+++ BaekJoon/src/Programmers/S53.java
@@ -1,8 +1,5 @@
 package Programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class S53 {
 	public static void main(String[] args) {
 		int rows = 3;
@@ -15,63 +12,58 @@ public class S53 {
 	
 	public static int[] solution(int rows, int columns, int[][] queries) {
         
-		int[][] origin = new int[rows+1][columns+1];
+		int[][] origin = new int[rows][columns];
+		int[] answer = new int[queries.length];
+		int count = 1;
 		
-		for(int i=1;i<origin.length;i++) {
-			for(int j=1;j<origin[i].length;j++) {
-				origin[i][j] = ((i-1) * columns + j);
-				System.out.println(origin[i][j]);
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<columns;j++) {
+				origin[i][j] = count;
+				count++;
 			}
 		}
-		
-		System.out.println(Arrays.toString(origin));
 		
 		int x1 = 0;
 		int x2 = 0; 
 		int y1 = 0;
 		int y2 = 0;
 
-		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(int i=0;i<queries.length;i++) {
-			queries[i][0] = x1;
-			queries[i][1] = x2;
-			queries[i][2] = y1;
-			queries[i][3] = y2;
+			x1 = queries[i][0]-1;
+			y1 = queries[i][1]-1;
+			x2 = queries[i][2]-1;
+			y2 = queries[i][3]-1;
 			
-			int start = origin[x1][y1];
-			int val = 0;
-			int min = 0;
+			int min = origin[x1][y1];
+			int val = origin[x1][y1];
 			
-			for(int j=x1;j<=x2;j++) {
-				for(int y=y1;y<=y2;y++) {
-					
-					if(j==x1 || y == y1) {
-						
-					}
-					val = start;
-					
-				}
+			//x가 이례적으로 세로축임 따라서 세로쪽부터
+			for(int j=x1;j<x2;j++) {
+				origin[j][y1] = origin[j+1][y1];
+				min = Math.min(origin[j][y1], min);
 			}
 			
+			for(int j=y1;j<y2;j++) {
+				origin[x2][j] = origin[x2][j+1];
+				min = Math.min(origin[x2][j], min);
+			}
 			
+			for(int j=x2;j>x1;j--) {
+				origin[j][y2] = origin[j-1][y2];
+				min = Math.min(origin[j][y2], min);
+			}
+			
+			for(int j=y2;j>y1;j--) {
+				origin[x1][j] = origin[x1][j-1];
+				min = Math.min(origin[x1][j], min);
+			}
+			
+			origin[x1][y1+1] = val;
+			answer[i] = min;
 			
 		}
 		
+		return answer;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		int[] answer = {};
-        
-        
-        
-        return answer;
     }
 }
